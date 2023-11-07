d874ddf26877b128550792a63ba93a040d1871dd
Shian-Li Kelly-Williams <shian-li.kelly-williams@magd.ox.ac.uk>
2023-11-07 14:31:04 +0000

    Hi

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,11 @@
 #Script to plot data and model
+#first model evaluates when t is small => exploring exponential growth; gives r and No
+#second model evaluates when No is constant, gives K
+#comparing actual data relationship to modelled relationship
+# => assessing the fit of linear model to non-linear data
 
-growth_data <- read.csv("???")
+
+growth_data <- read.csv("experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +15,19 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 7.1855445 #this is the intercept from assessing model1 (intercept); when t = 0
   
-r <- ??? #
+r <- 0.0095218 #this is the slope from assessing model1 (t)
   
-K <- ??? #
+K <- 5.979e+10 #this is the intercept from assessing model2 (the plateau)
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
-  geom_function(fun=logistic_fun, colour="red") +
+  geom_function(fun=logistic_fun, colour="red") + #adding our model
   
-  geom_point()
+  geom_point()+
+  scale_y_continuous(trans='log10')
 
-  #scale_y_continuous(trans='log10')
+#almost a good fit
 
 
